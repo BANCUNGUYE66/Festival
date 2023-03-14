56aad78331ec90937b7d8755e6a968754e82b8ac
Aimable BANCUNGUYE <bancunguye66@gmail.com>
2023-03-15 00:31:14 +0300

    Solve error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const humburger = document.querySelector('.humburger_menu');
 
 humburger.addEventListener('click', () => {
-  const hell = []
-  hell.push(1)
-})
+  const hell = [];
+  hell.push(1);
+});
